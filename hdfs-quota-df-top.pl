cd0eba12e75cd072005a0c173b58c6b20e397ae5
Nikhil Mulley <nikhil.mulley@inmobi.com>
2012-09-02 01:55:36 +0530

    added support to emit metrics to ganglia monitoring

--- hdfs-quota-df-top.pl
+++ hdfs-quota-df-top.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 #
-# $Id: hdfs-quota-df-top.pl 9257 2012-06-08 16:53:53Z nikhil.mulley $
+# $Id: hdfs-quota-df-top.pl 12438 2012-08-27 03:59:28Z nikhil.mulley $
 # $Author: nikhil.mulley $
+# $HeadURL:$
 # 
 # Description: report the top quota usage directories in HDFS
 #
@@ -12,12 +13,14 @@ use warnings;
 use Getopt::Long qw(:config no_ignore_case);
 use File::Basename;
 use Number::Bytes::Human qw(format_bytes);
+use Parallel::ForkManager;
 use Pod::Usage;
 use Data::Dumper qw (Dumper);
 
 # globals
 use vars qw ($PROG $PROGLOG $HADOOP_CMD @HDFS_NAMESPACE $TOP_LIMIT
-             $hdfs_topdir @hdfs_abs_dirs $verbose %hdfs_quota_info $sort_by_col %cols);
+             $hdfs_topdir @hdfs_abs_dirs $verbose %hdfs_quota_info $sort_by_col 
+             $hadoop_conf %cols $GANGLIA);
 
 # main
 &initialization;
@@ -29,8 +32,17 @@ if ( defined $hdfs_topdir ) {
     $hdfs_topdir = "/user";
 }
 
+print "INFO | processing quota information for directories under $hdfs_topdir  \n" if $verbose;
+
 &get_hdfs_quota_info($hdfs_topdir);
-&report_hdfs_top_df;
+
+if ($GANGLIA) {
+   print "INFO| Running in ganglia mode \n" if $verbose;
+   die "ERROR| Which Hadoop grid is this run for? \n" if !defined ($hadoop_conf);
+   &inform_ganglia;
+} else {
+   &report_hdfs_top_df;
+}
 exit;
 
 # end of main
@@ -44,6 +56,7 @@ sub initialization {
     $PROG = basename($0);
     $PROGLOG = "/var/tmp/$PROG.log";
     $TOP_LIMIT = 10;
+    $hadoop_conf = `facter hadoop_conf`; chomp $hadoop_conf;
     %hdfs_quota_info = ();
     %cols = (
 #            'name'       => 'HDFS_DIRECTORY',
@@ -63,6 +76,7 @@ sub parse_opts {
         'path:s{0,9}' => \@hdfs_abs_dirs,
         'sortby=s' => \$sort_by_col,
         'l=i' => \$TOP_LIMIT,
+        'ganglia' => \$GANGLIA,
         'verbose' => \$verbose,
         'help|h|?' => \$help
     ) or pod2usage(2);
@@ -186,6 +200,63 @@ $directory, $allocation, $hdfs_used, $percent_used
 }
 
 
+# emit metrics to ganglia for graphing
+sub inform_ganglia {
+   my ($hdfs_directory, $hdfs_allocation, $hdfs_used) = ();
+   my ($iterator) = 0;
+   my ($metric_step) = 60 * 15; # 15 minutes
+   my ($MAX_GMETRIC_PROC) = 5;
+   if (%hdfs_quota_info) {
+
+   my @hdfs_dirs_list = keys %hdfs_quota_info;
+
+   if ( @hdfs_abs_dirs && grep { /$hdfs_topdir/ } @hdfs_abs_dirs ) {
+     @hdfs_dirs_list = ();
+     foreach ( @hdfs_abs_dirs ) {
+       chomp;
+       push ( @hdfs_dirs_list, $_ ) if ( defined($hdfs_quota_info{$_}) ) ;
+     }
+   }
+
+   my $gm_pm = new Parallel::ForkManager($MAX_GMETRIC_PROC);
+
+   $gm_pm->run_on_finish(
+		   sub { my ($pid, $exit_code, $ident) = @_;
+		   print "***** $ident just got finished ".
+		   "with PID $pid and exit code: $exit_code\n" if $verbose;
+		   }
+		   );
+
+   $gm_pm->run_on_start(
+		   sub { my ($pid,$ident)=@_;
+		   print "***** $ident just started, pid: $pid\n" if $verbose;
+		   }
+		   );
+
+   foreach my $hdfs_quota_dir ( @hdfs_dirs_list ) {
+	   $gm_pm->start($hdfs_quota_dir) and next; # do the fork
+
+           ($hdfs_directory = $hdfs_quota_dir) =~ s/\//_/g; 
+           $hdfs_allocation   = trim($hdfs_quota_info{$hdfs_quota_dir}{'SPACE_QUOTA'});
+           $hdfs_used    = trim($hdfs_quota_info{$hdfs_quota_dir}{'USED_SPACE'});
+           my $alloc_metric_cmd = "gmetric -n \"${hadoop_conf}.quota.allocation.${hdfs_directory}\" -v ${hdfs_allocation} -u Kilobytes -t int32 -d ${metric_step} ";
+           my $used_metric_cmd =  "gmetric -n \"${hadoop_conf}.quota.usage.${hdfs_directory}\" -v ${hdfs_used} -u Kilobytes -t int32 -d ${metric_step} ";
+           print "INFO| Running $alloc_metric_cmd \n" if $verbose;
+           system($alloc_metric_cmd);
+           print "INFO| Running $used_metric_cmd \n" if $verbose;
+           system($used_metric_cmd);
+    
+           $gm_pm->finish;
+           #$iterator += 1;
+           #last if $iterator == $TOP_LIMIT && $TOP_LIMIT != -1;;
+           
+     } # end of foreach
+   $gm_pm->wait_all_children; # have all forked process exited?
+   } # end if
+
+}
+
+# trim whitespaces
 sub trim {
 
  my ($a) = (@_);
@@ -239,6 +310,10 @@ Provide the hdfs top level directory name space to query the HDFS quota director
 Provide the number to limit the output to top -l number of lines.
   Default value is 10 unless the option is specified.
   -1 for imposing no limits on the output.
+
+=item B<-ganglia>
+
+Emit ganglia metrics using gmetric for HDFS Quota allocation/usage of directories.
 
 =item B<-sortby>
 
