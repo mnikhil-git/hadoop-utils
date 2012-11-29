d885e576141677f39bc15c11eeda6e92c4716293
mnikhil-git <mnikhil@gmail.com>
2012-11-29 14:12:50 +0530

    Update bin/JobsPS.sh

--- bin/JobsPS.sh
+++ bin/JobsPS.sh
@@ -2,8 +2,8 @@
 # Author: Nikhil Mulley
 # 
 # 20121228
-export HADOOP_CLASSPATH=/usr/lib/hadoop/hadoop-core-0.20.2-cdh3u3.jar:/usr/lib/hadoop/hadoop-tools-0.20.2-cdh3u3.jar:/usr/lib/hadoop-0.20/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20/lib/log4j-1.2.15.jar:/usr/lib/hadoop-0.20/lib/commons-logging-1.0.4.jar:../jar/.
-export CLASSPATH=/usr/lib/hadoop/hadoop-core-0.20.2-cdh3u3.jar:/usr/lib/hadoop/hadoop-tools-0.20.2-cdh3u3.jar:/usr/lib/hadoop-0.20/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20/lib/log4j-1.2.15.jar:/usr/lib/hadoop-0.20/lib/commons-logging-1.0.4.jar:../jar/.
+export HADOOP_CLASSPATH=/usr/lib/hadoop/hadoop-core-0.20.2-cdh3u3.jar:/usr/lib/hadoop/hadoop-tools-0.20.2-cdh3u3.jar:/usr/lib/hadoop-0.20/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20/lib/log4j-1.2.15.jar:/usr/lib/hadoop-0.20/lib/commons-logging-1.0.4.jar:../java/.
+export CLASSPATH=/usr/lib/hadoop/hadoop-core-0.20.2-cdh3u3.jar:/usr/lib/hadoop/hadoop-tools-0.20.2-cdh3u3.jar:/usr/lib/hadoop-0.20/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20/lib/log4j-1.2.15.jar:/usr/lib/hadoop-0.20/lib/commons-logging-1.0.4.jar:../java/.
 export HADOOP_HOME=/usr/lib/hadoop
 
 JCLASSNAME="JobsPS"
