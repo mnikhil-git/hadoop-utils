d7d3a91b4aa7057224c213dc5e5417c42243f7c3
Nikhil Mulley <nikhil.mulley@inmobi.com>
2012-11-29 14:04:12 +0530

    added new tool. Hadoop Job Lister -- improved with more options

---
+++ bin/JobsPS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Author: Nikhil Mulley
+# 
+# 20121228
+export HADOOP_CLASSPATH=/usr/lib/hadoop/hadoop-core-0.20.2-cdh3u3.jar:/usr/lib/hadoop/hadoop-tools-0.20.2-cdh3u3.jar:/usr/lib/hadoop-0.20/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20/lib/log4j-1.2.15.jar:/usr/lib/hadoop-0.20/lib/commons-logging-1.0.4.jar:../jar/.
+export CLASSPATH=/usr/lib/hadoop/hadoop-core-0.20.2-cdh3u3.jar:/usr/lib/hadoop/hadoop-tools-0.20.2-cdh3u3.jar:/usr/lib/hadoop-0.20/lib/commons-cli-1.2.jar:/usr/lib/hadoop-0.20/lib/log4j-1.2.15.jar:/usr/lib/hadoop-0.20/lib/commons-logging-1.0.4.jar:../jar/.
+export HADOOP_HOME=/usr/lib/hadoop
+
+JCLASSNAME="JobsPS"
+
+/usr/bin/hadoop ${JCLASSNAME} "$@"
